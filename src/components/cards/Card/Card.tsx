d686bea5aa056da8546e66998faba7fabb653b4e
nikitakukharchuk <nikitakukharchuk@mail.ru>
2022-12-19 15:28:25 +0300

    init ( add adaptive card ) : #Main_page_cards

--- src/components/cards/Card/Card.tsx
+++ src/components/cards/Card/Card.tsx
@@ -11,16 +11,16 @@ type PropsCardType = {
 export const Card: FC<PropsCardType> = ({title, date, userName, status}) => {
 
     return (
-        <div className='border rounded-xl shadow-md px-10 pt-10 pb-10'>
-            <div>{status && "verifyed"}</div>
+        <div className='border rounded-xl shadow-xl px-10 py-10 relative'>
+            <div className='border rounded-xl shadow-md absolute px-5 bg-sky-400 text-white top-0 right-5'>{status && "verifyed"}</div>
             <div className='grid justify-items-center'>
-                <h3 className='mb-10'>NodeJs</h3>
+                <h3>NodeJs</h3>
                 <p>{title}</p>
-                <p><span>By </span>{userName}</p>
-                <Button className='my-10' variant="contained" href="#contained-buttons">
+                <p className='mt-0'><span className='text-slate-400'>By </span>{userName}</p>
+                <Button  variant="contained" href="#contained-buttons">
                     Start
                 </Button>
-                <p>Created:{date}</p>
+                <p><span className='text-slate-400'>Created: </span>{date}</p>
             </div>
         </div>)
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        '4': 'repeat(auto-fill, minmax(300px, 1fr))',
+      }
+    },
   },
   plugins: [],
 }
