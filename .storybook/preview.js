28df2edada9db6e4bb162f0e42f56edcee4283db
admin <krivalcevich@gmail.com>
2022-12-21 22:11:31 +0300

    Bug Fix with StoryBook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,22 @@
 import * as NextImage from "next/image";
 import 'tailwindcss/tailwind.css';
+import '../src/styles/globals.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '@fontsource/material-icons';
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import {defaultTheme} from "../src/styles/theme/defaultTheme";
+export const withMuiTheme = (Story) => (
+    <ThemeProvider theme={defaultTheme}>
+        <CssBaseline />
+        <Story />
+    </ThemeProvider>
+);
+
+export const decorators = [withMuiTheme];
+
 
 
 const OriginalNextImage = NextImage.default;
