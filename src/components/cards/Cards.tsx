5e46365ff8e07b17ceae014eacb8524c031ce13f
admin <krivalcevich@gmail.com>
2022-12-22 18:16:34 +0300

    [14]was_created_story_for_cards

--- src/components/cards/Cards.tsx
+++ src/components/cards/Cards.tsx
@@ -1,13 +1,10 @@
 import React, {FC} from 'react';
 import {Card} from "./Card/Card";
-
 import {CardsType, OneCardType} from "../../Types/CardTypes";
 
 type PropsCardsType = {
-cards: CardsType
+    cards: CardsType
 }
-
-
 
 
 export const Cards: FC<PropsCardsType> = ({cards}) => {
@@ -15,7 +12,7 @@ export const Cards: FC<PropsCardsType> = ({cards}) => {
 
     return (
         <div className='grid grid-cols-4 gap-20 px-20'>
-            {cards&&cards.map(({id, title, userName, date, status}:OneCardType) =>
+            {cards.map(({id, title, userName, date, status}: OneCardType) =>
                 <Card key={id} title={title} userName={userName} date={date} status={status}/>)}
         </div>
     );
