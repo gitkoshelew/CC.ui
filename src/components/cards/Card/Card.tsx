b6315e3bddf73b17bf4b77587370e2fd942b0515
nikitakukharchuk <nikitakukharchuk@mail.ru>
2022-12-22 23:04:40 +0300

    add_styles_cards

--- src/components/cards/Card/Card.tsx
+++ src/components/cards/Card/Card.tsx
@@ -1,5 +1,6 @@
 import React, {FC} from 'react';
 import {Button, Typography} from "@mui/material";
+import s from "./Card.module.css"
 
 type PropsCardType = {
     title: string
@@ -13,18 +14,18 @@ export const Card: FC<PropsCardType> = ({title, date, userName, status}) => {
     return (
         <div className='text-center bg-background-paper shadow border rounded-2xl px-5 pt-10 pb-2 relative'>
             {status &&
-                <div className='absoluteCard bg-secondary-main text-secondary-contrastText'>
+                <div className={`${s.absoluteVerified} bg-secondary-main text-secondary-contrastText`}>
                     verified
                 </div>
             }
             <div className='grid justify-items-center'>
                 <Typography fontWeight={"600"}>NodeJs</Typography>
-                <p>{title}</p>
-                <p className='mt-0'><span className='text-text-primaryAlpha300'>By </span>{userName}</p>
+                <p className='mt-7 mb-1'>{title}</p>
+                <p className='mt-0 mb-7'><span className='text-text-primaryAlpha300'>By </span>{userName}</p>
                 <Button href="#contained-buttons">
                     Start
                 </Button>
-                <p><span className='text-text-primaryAlpha300'>Created: </span>{date}</p>
+                <p className='mb-8'><span className='text-text-primaryAlpha300'>Created: </span>{date}</p>
             </div>
         </div>)
 
--- src/components/cards/Cards.tsx
+++ src/components/cards/Cards.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Card} from "./Card/Card";
 import {cards} from "../../Mocs/Moc";
+import s from "./Cards.module.css"
 
 export const Cards = () => {
 
     return (
-        <div className='grid grid-cols-4 gap-7'>
+        <div className={s.Cards}>
             {cards.map(({id, title, userName, date, status}) =>
                 <Card key={id} title={title} userName={userName} date={date} status={status}/>)}
         </div>
