a3224aa49bc17f776c19d1973fb9eb008590ee36
nikitakukharchuk <nikitakukharchuk@mail.ru>
2022-12-26 12:39:32 +0300

    add_styles_cards

--- src/components/cards/Card/Card.tsx
+++ src/components/cards/Card/Card.tsx
@@ -12,7 +12,7 @@ export const Card: FC<PropsCardType> = ({ title, date, userName, status }) => (
   <div className='flex flex-col text-center bg-background-paper shadow border rounded-2xl px-5 pt-10 pb-5 relative'>
     {status && (
       <div
-        className= 'absolute -top-3.5 right-5 py-1 px-7 rounded-2xl text-sm font-light bg-secondary-main text-secondary-contrastText'
+        className= 'absolute -top-3.5 right-5 py-1 px-5 rounded-2xl text-sm font-light bg-secondary-main text-secondary-contrastText'
       >
         verified
       </div>
