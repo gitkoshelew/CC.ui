7407d53eff80247390b10008a48799637cae7f22
nikitakukharchuk <nikitakukharchuk@mail.ru>
2023-01-05 11:32:05 +0300

    #42_1_create_moc_table_result

---
+++ src/Mocs/TableResultMoc.ts
@@ -0,0 +1,15 @@
+export type ResultTableDataType = {
+    id: number;
+    incorrectAnswer: string
+};
+
+export const ResultTableData: ResultTableDataType[] = [
+    {
+        id: 1,
+        incorrectAnswer: 'Lorem ipsum'
+    },
+    {
+        id: 2,
+        incorrectAnswer: 'Lorem ipsum'
+    }
+]
