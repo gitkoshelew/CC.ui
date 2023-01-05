561c3e80f33da07a0c4937d7fc5a9101dd944771
nikitakukharchuk <nikitakukharchuk@mail.ru>
2023-01-05 10:03:03 +0300

    fix_conflicts

--- src/components/cards/Card/Card.tsx
+++ src/components/cards/Card/Card.tsx
@@ -12,21 +12,22 @@ export const Card: FC<PropsCardType> = ({ title, date, userName, status }) => (
   <div className='flex flex-col text-center bg-background-paper shadow border rounded-2xl px-5 pt-10 pb-5 relative'>
     {status && (
       <div
-        className= 'absolute -top-3.5 right-5 py-1 px-5 rounded-2xl text-sm font-light bg-secondary-main text-secondary-contrastText'
-      >
+        className= 'absolute -top-3.5 right-5 py-1 px-7 rounded-2xl text-sm font-light bg-secondary-main text-secondary-contrastText'>
         verified
       </div>
     )}
-      <Typography className='mb-8' fontWeight='600'>NodeJs</Typography>
-      <div className='grow grid content-center'>
+    <div className='grid justify-items-center'>
+      <Typography className='mb-8' fontWeight='600'>
+        NodeJs
+      </Typography>
       <p className='mb-1 mt-0'>{title}</p>
       <p className='mt-0 mb-7'>
         <span className='text-text-primaryAlpha300'>By </span>
         {userName}
       </p>
-    </div>
-    <div>
-      <Button className='mb-4' href='#contained-buttons'>Start</Button>
+      <Button className='mb-4' href='#contained-buttons'>
+        Start
+      </Button>
       <p className='mb-3 mt-0'>
         <span className='text-text-primaryAlpha300'>Created: </span>
         {date}
