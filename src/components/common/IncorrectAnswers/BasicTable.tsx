91fbbde5f7e0ce13225f5328383d87f664004b5b
nikitakukharchuk <nikitakukharchuk@mail.ru>
2023-01-08 13:39:48 +0300

    Add_table

--- src/components/common/IncorrectAnswers/BasicTable.tsx
+++ src/components/common/IncorrectAnswers/BasicTable.tsx
@@ -6,44 +6,52 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import {ResultTableDataType} from "../../../Mocs/TableResultMoc";
-import {FC} from "react";
+import { FC } from 'react';
+import { ResultTableDataType } from '../../../Mocs/TableResultMoc';
 
 type IncorrectAnswerPropsType = {
-    IncorrectAnswers: ResultTableDataType[]
-}
-export const BasicTable:FC<IncorrectAnswerPropsType>=({IncorrectAnswers}) => {
-    return (
-        <TableContainer component={Paper}>
-            <Table>
-                <TableHead sx={{
-                    backgroundSize: 'cover',
-                    bgcolor: 'primary.main',
-                    color: 'primary.contrastText',
-                    borderRadius: 2,
-                }}>
-                    <TableRow>
-                        <TableCell sx={{
-                            color: 'primary.contrastText',
-                        }}>Question</TableCell>
-                        <TableCell sx={{
-                            color: 'primary.contrastText',
-                        }}>Incorrect Answer</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {IncorrectAnswers.map((row) => (
-                        <TableRow
-                            key={row.id}
-                        >
-                            <TableCell component="th" scope="row">
-                                {row.id}
-                            </TableCell>
-                            <TableCell>{row.incorrectAnswer}</TableCell>
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    );
-}
+  IncorrectAnswersList: ResultTableDataType[];
+};
+export const BasicTable: FC<IncorrectAnswerPropsType> = ({
+  IncorrectAnswersList,
+}) => (
+  <TableContainer component={Paper}>
+    <Table>
+      <TableHead
+        sx={{
+          backgroundSize: 'cover',
+          bgcolor: 'primary.main',
+          color: 'primary.contrastText',
+          borderRadius: 2,
+        }}
+      >
+        <TableRow>
+          <TableCell
+            sx={{
+              color: 'primary.contrastText',
+            }}
+          >
+            Question
+          </TableCell>
+          <TableCell
+            sx={{
+              color: 'primary.contrastText',
+            }}
+          >
+            Incorrect Answer
+          </TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {IncorrectAnswersList.map((row) => (
+          <TableRow key={row.id}>
+            <TableCell component='th' scope='row'>
+              {row.id}
+            </TableCell>
+            <TableCell>{row.incorrectAnswer}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </TableContainer>
+);
--- src/stories/Organism/incorrectAnswer.stories.tsx
+++ src/stories/Organism/incorrectAnswer.stories.tsx
@@ -1,12 +1,10 @@
-import {IncorrectAnswers} from "../../components/common/IncorrectAnswers/incorrectAnswers";
-import {ResultTableData} from "../../Mocs/TableResultMoc";
+import { IncorrectAnswers } from '../../components/common/IncorrectAnswers/incorrectAnswers';
+import { ResultTableData } from '../../Mocs/TableResultMoc';
 
-export const Default = () => <IncorrectAnswers IncorrectAnswers={ResultTableData}/>
+export const Default = () => (
+  <IncorrectAnswers IncorrectAnswersList={ResultTableData} />
+);
 export default {
-    title: 'Organism/IncorrectAnswer',
-    component: Default,
-}
-
-
-
-
+  title: 'Organism/IncorrectAnswer',
+  component: Default,
+};
