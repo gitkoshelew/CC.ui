f859880650c9803a95f3b6fddfe4d4855554cedb
nikitakukharchuk <nikitakukharchuk@mail.ru>
2023-01-09 16:18:17 +0300

    style_table_header

--- src/components/common/IncorrectAnswers/BasicTable.tsx
+++ src/components/common/IncorrectAnswers/BasicTable.tsx
@@ -15,33 +15,43 @@ type IncorrectAnswerPropsType = {
 export const BasicTable: FC<IncorrectAnswerPropsType> = ({
   IncorrectAnswersList,
 }) => (
-  <TableContainer component={Paper}>
-    <Table>
-      <TableHead
+  <>
+    <TableContainer component={Paper} className='my-10'>
+      <Table
         sx={{
-          backgroundSize: 'cover',
           bgcolor: 'primary.main',
-          color: 'primary.contrastText',
-          borderRadius: 2,
         }}
       >
-        <TableRow>
-          <TableCell
-            sx={{
-              color: 'primary.contrastText',
-            }}
-          >
-            Question
-          </TableCell>
-          <TableCell
-            sx={{
-              color: 'primary.contrastText',
-            }}
-          >
-            Incorrect Answer
-          </TableCell>
-        </TableRow>
-      </TableHead>
+        <TableHead
+          sx={{
+            backgroundSize: 'cover',
+            bgcolor: 'primary.main',
+            color: 'primary.contrastText',
+            borderRadius: 2,
+          }}
+        >
+          <TableRow>
+            <TableCell
+              sx={{
+                color: 'primary.contrastText',
+                width: '200px',
+                borderBottom: 'none',
+              }}
+            >
+              Question
+            </TableCell>
+            <TableCell
+              sx={{
+                color: 'primary.contrastText',
+                borderBottom: 'none',
+              }}
+            >
+              Incorrect Answer
+            </TableCell>
+          </TableRow>
+        </TableHead>
+      </Table>
+      {/* <Table>
       <TableBody>
         {IncorrectAnswersList.map((row) => (
           <TableRow key={row.id}>
@@ -52,6 +62,63 @@ export const BasicTable: FC<IncorrectAnswerPropsType> = ({
           </TableRow>
         ))}
       </TableBody>
-    </Table>
-  </TableContainer>
+    </Table> */}
+    </TableContainer>
+    <TableContainer component={Paper}>
+      {/* <Table>
+        <TableHead
+          sx={{
+            backgroundSize: 'cover',
+            bgcolor: 'primary.main',
+            color: 'primary.contrastText',
+            borderRadius: 2,
+          }}
+        >
+          <TableRow>
+            <TableCell
+              sx={{
+                color: 'primary.contrastText',
+              }}
+            >
+              Question
+            </TableCell>
+            <TableCell
+              sx={{
+                color: 'primary.contrastText',
+              }}
+            >
+              Incorrect Answer
+            </TableCell>
+          </TableRow>
+        </TableHead>
+      </Table> */}
+      <Table>
+        <TableBody>
+          {IncorrectAnswersList.map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{
+                display: 'flex',
+              }}
+            >
+              <div>
+                <TableCell
+                  component='th'
+                  scope='row'
+                  sx={{
+                    width: '200px',
+                  }}
+                >
+                  {row.id}
+                </TableCell>
+              </div>
+              <div>
+                <TableCell>{row.incorrectAnswer}</TableCell>
+              </div>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  </>
 );
