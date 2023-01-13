96501741d54c77999b95882a5da13aeaf0af1fcc
Zenater <zent230096@mail.ru>
2023-01-13 17:06:38 +0300

    fix:  merge dev in my branch

--- src/tests/index/index.test.tsx
+++ src/tests/index/index.test.tsx
@@ -1,9 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import Home from '../../pages/index';
+import { cards } from '../../Mocs/CardMoc';
 
 describe('Home', () => {
   it('renders a heading', () => {
-    const { container } = render(<Home />);
+    const { container } = render(<Home quizes={cards} />);
 
     const heading = screen.getByRole('heading', {
       name: /welcome to next\.js!/i,
