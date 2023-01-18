54d2b1b33afb9f16ed483acfc23991658c27aa34
admin <krivalcevich@gmail.com>
2023-01-18 12:48:33 +0300

    [89]feature: add unit test for questions reducer

---
+++ src/tests/reducers/questions-reducer.test.ts
@@ -0,0 +1,40 @@
+import { TestQuestionsType } from '../../Types/TestQuestionsType';
+import {
+  fetchQuestionsAC,
+  questionsReducer,
+} from '../../store/reducers/questions-reducer';
+
+test('correct date adding to the initialState ', () => {
+  const startState = { questions: [] as TestQuestionsType[] };
+
+  const action = fetchQuestionsAC([
+    {
+      id: 1,
+      title: 'Question 1 JavaScript',
+      content: {
+        options: ['Answer1', 'Answer2', 'Answer3', 'Answer4'],
+      },
+      type: 'One correct answer',
+      difficulty: 'Easy',
+      description: 'How do you write "Hello World" in an alert box?',
+      correctAnswer: 'Answer1',
+      topic: 'Theme 2',
+    },
+    {
+      id: 2,
+      title: 'Question 2 JavaScript',
+      content: {
+        options: ['Answer4', 'Answer5', 'Answer6', 'Answer7'],
+      },
+      type: 'One correct answer',
+      difficulty: 'Medium',
+      description: 'What is a prototype chain?',
+      correctAnswer: 'Answer2',
+      topic: 'Theme 2',
+    },
+  ]);
+  const endState = questionsReducer(startState, action);
+
+  expect(endState.questions.length).toBe(2);
+  expect(endState.questions[1].title).toBe('Question 2 JavaScript');
+});
