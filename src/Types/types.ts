9cf166ba295309f460df37be58db073a4bd1c2f2
nikitakukharchuk <nikitakukharchuk@mail.ru>
2023-02-20 15:55:16 +0300

    delete registration type

--- src/Types/types.ts
+++ src/Types/types.ts
@@ -3,9 +3,3 @@ import { ReactNode } from 'react';
 export type WithChild = {
   children?: ReactNode;
 };
-export type RegistrationType = {
-  name: string;
-  email: string;
-  password: string;
-  nickname: string;
-};
